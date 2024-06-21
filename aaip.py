92b4318e684ba029267c5e51e3714e7fd5eebd25
sagardibyacodes <167241118+sagardibyacodes@users.noreply.github.com>
2024-06-21 15:20:16 +0530

    Add files via upload

---
+++ aaip.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import ollama
+
+class AI_Assistant:
+    def main(self):
+        st.title("GetAnswer gpt")
+        full_transcript = [{"role": "system", "content": "Sagar."}]
+        user_input = st.text_input("Ask here:")
+        if st.button("Send"):
+            full_transcript.append({"role": "user", "content": user_input})
+            response = ollama.chat(
+                model="llama3",
+                messages=full_transcript,
+                stream=True,
+            )
+            full_response = ""
+            for chunk in response:
+                full_response += chunk['message']['content']
+                if chunk['message']['content'].endswith('.'):
+                    st.write(full_response)
+                    full_response = ""
+
+ai_assistant = AI_Assistant()
+ai_assistant.main()
